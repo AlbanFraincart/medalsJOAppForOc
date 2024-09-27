bc0fe2fc80713dd7cef1d3fe532cb5527d02f05c
Fraincart Alban <alban.fraincart@hotmail.com>
2024-09-27 15:28:53 +0200

    initial commit

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { OlympicCountry } from 'src/app/core/models/Olympic';
 import { OlympicService } from 'src/app/core/services/olympic.service';
 
 @Component({
@@ -8,11 +9,12 @@ import { OlympicService } from 'src/app/core/services/olympic.service';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  public olympics$: Observable<any> = of(null);
+  public olympics$: Observable<OlympicCountry[] | null> | undefined;
 
-  constructor(private olympicService: OlympicService) {}
+  constructor(private olympicService: OlympicService) { }
 
   ngOnInit(): void {
     this.olympics$ = this.olympicService.getOlympics();
+    console.log('this.olympics$', this.olympics$);
   }
 }
