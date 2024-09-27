c526466221b30d8e5eda1bb087930a386e25451b
Fraincart Alban <alban.fraincart@hotmail.com>
2024-09-27 16:41:39 +0200

    graphic installed and configured

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable, Subscription } from 'rxjs';
 import { OlympicCountry } from 'src/app/core/models/Olympic';
 import { OlympicService } from 'src/app/core/services/olympic.service';
 
@@ -8,13 +9,62 @@ import { OlympicService } from 'src/app/core/services/olympic.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   public olympics$: Observable<OlympicCountry[] | null> | undefined;
+  public chartData: any[] = [];
+  public view: [number, number] = [700, 400];
+  public colorScheme = 'cool';
+  private subscription: Subscription = new Subscription();
+  public totalCountries: number = 0;
+  public totalMedals: number = 0;
 
-  constructor(private olympicService: OlympicService) { }
+
+  constructor(private olympicService: OlympicService, private router: Router) { }
 
   ngOnInit(): void {
     this.olympics$ = this.olympicService.getOlympics();
-    console.log('this.olympics$', this.olympics$);
+    this.olympics$.subscribe((olympics) => {
+      if (olympics) {
+        this.prepareChartData(olympics);
+        this.calculateTotals(olympics);
+      }
+    });
   }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+
+  prepareChartData(olympics: OlympicCountry[]): void {
+    this.chartData = olympics.map((country) => {
+      const totalMedals = country.participations.reduce(
+        (sum, participation) => sum + participation.medalsCount,
+        0
+      );
+      return {
+        name: country.country,
+        value: totalMedals,
+        id: country.id, // for navigation
+      };
+    });
+  }
+
+  onSelect(data: any): void {
+    const country = this.chartData.find((item) => item.name === data.name);
+    if (country) {
+      this.router.navigate(['/country', country.id]);
+    }
+  }
+
+  calculateTotals(olympics: OlympicCountry[]): void {
+    this.totalCountries = olympics.length;
+    this.totalMedals = olympics.reduce((sum, country) => {
+      return sum + country.participations.reduce(
+        (countrySum, participation) => countrySum + participation.medalsCount,
+        0
+      );
+    }, 0);
+  }
+
 }
